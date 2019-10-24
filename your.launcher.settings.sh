dcb14c893a08ae6815c6ab36d061b72df7663ac0
kazajhodo <acornell@augustash.com>
2019-10-24 13:43:54 -0500

    Added settings file.
    User settings are now set in your.launcher.settings.sh.
    
    This file is automatically changed into .launcher.settings.sh, which is used for settings.

---
+++ your.launcher.settings.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# IDE
+# The value should be what you enter to open a file in your ide via commandline.
+ide='code'
+
+# PROJECTS DIRECTORY
+# This is where all of your local sites are location.
+# Usually user root 'sites', or 'projects'
+# !! Case sensitive !!
+projectsDirectory='Projects'
+
+# BROWSER
+# Now uses default browser.
