13615d1688710845204e6490122a50821e3cc46d
kazajhodo <acornell@augustash.com>
2020-03-23 13:29:52 -0500

    Changed cache setting.
    Turned off render cache in settings.local.php.

--- base/settings.local.php
+++ base/settings.local.php
@@ -66,7 +66,7 @@ $config['system.performance']['js']['preprocess'] = FALSE;
  *
  * Only use this setting once the site has been installed.
  */
-// $settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['render'] = 'cache.backend.null';
 
 /**
  * Disable caching for migrations.
